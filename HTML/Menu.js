40afcebadf03164a309e013ed205f2cc1d0afba8
Sandoru <159sandro159@gmail.com>
2022-11-18 20:05:03 -0500

    index -480px

---
+++ HTML/Menu.js
@@ -0,0 +1,6 @@
+let btnNav = document.getElementById('btnNav');
+let MenuNav = document.getElementById('MenuNav');
+btnNav.addEventListener('click', function(){
+    'use strict';
+    MenuNav.classList.toggle('mostrar');
+});
