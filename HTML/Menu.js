36899bbb7c78eebcbec725b58d4df3863244e977
Sandoru <159sandro159@gmail.com>
2023-01-17 22:02:17 -0500

    mejorando iniciar sesion

--- HTML/Menu.js
+++ HTML/Menu.js
@@ -4,3 +4,10 @@ btnNav.addEventListener('click', function(){
     'use strict';
     MenuNav.classList.toggle('mostrar');
 });
+
+let btndespops = document.getElementById('btndespops');
+let opscuenta = document.getElementById('opscuenta');
+btndespops.addEventListener('click', function(){
+    'use strict';
+    opscuenta.classList.toggle('mostrar2');
+});
