a8581eba68a8a7bfde4124a41d17ceb802891e46
Sandoru <159sandro159@gmail.com>
2023-01-22 18:54:05 -0500

    mejora de nav en celular

--- HTML/Menu.js
+++ HTML/Menu.js
@@ -1,9 +1,12 @@
 let btnNav = document.getElementById('btnNav');
 let MenuNav = document.getElementById('MenuNav');
+let idbody = document.getElementById('idbody');
 btnNav.addEventListener('click', function(){
     'use strict';
     MenuNav.classList.toggle('mostrar');
+    idbody.classList.toggle('parar');
 });
+
 
 let btndespops = document.getElementById('btndespops');
 let opscuenta = document.getElementById('opscuenta');
@@ -12,4 +15,11 @@ btndespops.addEventListener('click', function(){
     'use strict';
     opscuenta.classList.toggle('mostrar2');
     dropdownmenu.classList.toggle('dropdownmenu');
+});
+
+
+let Desaparecer = document.getElementById('Desaparecer');
+Desaparecer.addEventListener('click', function(){
+    'use strict';
+    MenuNav.classList.toggle('mostrar');
 });
